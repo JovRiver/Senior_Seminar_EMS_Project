d12cf8bf3512dfce15cb093dfb7934da2b052390
jovia <jovia@DESKTOP-U048VD3>
2021-07-08 23:27:59 -0400

    Began work on databasequerymanager.

--- Project-2-EMS/Models/DatabaseModels/DatabaseQueryManager.cs
+++ Project-2-EMS/Models/DatabaseModels/DatabaseQueryManager.cs
@@ -3,9 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Project_2_EMS.Models.PatientModels;
 
 namespace Project_2_EMS.Models.DatabaseModels {
     public class DatabaseQueryManager {
+        public Patient patient { get; }
+        public PatientAppointment appointment { get; }
+        public PatientPrescription prescription { get; }
+
+        public DatabaseQueryManager() { }
+
+        public void AppointmentQuery() {
+
+        }
+
+        public void PatientQuery() {
+
+        }
+
+        public void PrescriptionQuery() {
+
+        }
+
 
     }
 }
---
+++ Project-2-EMS/Models/PatientModels/PatientPrescription.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Project_2_EMS.Models.PatientModels {
+    public class PatientPrescription {
+        public int PrescriptionID { get; }
+        public int PatientID { get; }
+        public int VisitID { get; }
+        public String PrescriptionName { get; }
+        public String PrescriptionNotes { get; }
+        public int Refills { get; }
+
+        public PatientPrescription(int prescriptionID, int patientID, int visitID, String prescriptionName, String presciprtionNotes, int refills) {
+            PrescriptionID = prescriptionID;
+            PatientID = patientID;
+            VisitID = visitID;
+            PrescriptionName = prescriptionName;
+            PrescriptionNotes = presciprtionNotes;
+            Refills = refills;
+
+        }
+
+        public override string ToString() {
+            return "Prescription Name: " + PrescriptionName +
+                   "\n Doctor's Notes: " + PrescriptionNotes +
+                   "\n Refills Remaining: " + Refills + "\n\n\n";
+        }
+    }
+}

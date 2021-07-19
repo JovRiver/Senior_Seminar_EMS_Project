f616dc07986f7d1231026693ee6ba119a7a86680
jovia <jovia@DESKTOP-U048VD3>
2021-07-19 01:28:13 -0400

    Added a new test project to start testing the classes I created.

---
+++ Project-2-EMS_Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Project_2_EMS_Tests {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
